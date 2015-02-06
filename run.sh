4604af96dc5ecca66db4013efea6132aaeaa77d8
David Furnes <david@dfurnes.com>
2015-02-06 14:46:54 -0500

    Clean up script a bit.

--- run.sh
+++ run.sh
@@ -7,11 +7,6 @@ git commit -m "Build assets for v$PACKAGE_VERSION."
 echo "Listing tags on remote..."
 git ls-remote --tags $GIT_REMOTE
 
-echo "Listing matching tags on remote..."
-git ls-remote --tags $GIT_REMOTE | grep "v$PACKAGE_VERSION"
-
-echo "Attempting to create tag..."
-
 if ( git ls-remote --tags $GIT_REMOTE | grep "v$PACKAGE_VERSION" );
 then
   error 'Tag exists, skipping release.'
